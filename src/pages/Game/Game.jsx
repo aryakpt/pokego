0234b3b39f40fca8276c74edf5f00094bbb0ce9f
aryakpt <aryakrisna07@gmail.com>
2023-06-24 21:26:25 +0700

    disable warning

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { Container } from '../../components/layout';
 import { Button } from '../../components';
